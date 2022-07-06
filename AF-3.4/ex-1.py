40cf22c2374248c270faf399a14deb4010bab5e8
lucas_valduga <lukinhas.valduga@gmail.com>
2022-07-06 10:19:57 -0300

    ex1 resolvido

--- AF-3.4/ex-1.py
+++ AF-3.4/ex-1.py
@@ -10,29 +10,33 @@ def produtor():
       item = 'item ' + str(i)
     else:
       item = 'item ' + str(i+10)
-    with lock:
-      if len(buffer) == tam_buffer:
-        print(f'>>> Buffer cheio. Produtor {current_thread().name} ira aguardar.')
-        lugar_no_buffer.wait()    # aguarda que haja lugar no buffer
-      buffer.append(item)
-      print(f'{current_thread().name}: Produzido {item} (ha {len(buffer)} itens no buffer)')
-      item_no_buffer.notify()
+    with lock_prod:
+      with lock:
+        if len(buffer) == tam_buffer:
+          print(f'>>> Buffer cheio. Produtor {current_thread().name} ira aguardar.')
+          lugar_no_buffer.wait()    # aguarda que haja lugar no buffer
+        buffer.append(item)
+        print(f'{current_thread().name}: Produzido {item} (ha {len(buffer)} itens no buffer)')
+        item_no_buffer.notify()
 
 def consumidor():
   global buffer
   for i in range(10):
-    with lock:
-      if len(buffer) == 0:
-        print(f'>>> Buffer vazio. Consumidor {current_thread().name} ira aguardar.')
-        item_no_buffer.wait()   # aguarda que haja um item para consumir 
-      item = buffer.pop(0)
-      print(f'{current_thread().name}: Consumido {item} (ha {len(buffer)} itens no buffer)')
-      lugar_no_buffer.notify()
-    sleep(randint(0,2))         # fica um tempo consumindo...
+    with lock_cons:
+      with lock:
+        if len(buffer) == 0:
+          print(f'>>> Buffer vazio. Consumidor {current_thread().name} ira aguardar.')
+          item_no_buffer.wait()   # aguarda que haja um item para consumir 
+        item = buffer.pop(0)
+        print(f'{current_thread().name}: Consumido {item} (ha {len(buffer)} itens no buffer)')
+        lugar_no_buffer.notify()
+      sleep(randint(0,2))         # fica um tempo consumindo...
 
 buffer = []
 tam_buffer = 5
 lock = Lock()
+lock_prod = Lock()
+lock_cons = Lock()
 lugar_no_buffer = Condition(lock)
 item_no_buffer = Condition(lock)
 produtor1 = Thread(target=produtor,name="P1") 
